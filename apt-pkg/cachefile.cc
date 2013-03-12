4688fae0fd775460aa189b3bc3797afb078257a9
Michal Maruska <mmaruska@gmail.com>
2013-03-12 22:22:13 +0100

    revert the attempt to see progress in policy/pinning

--- apt-pkg/cachefile.cc
+++ apt-pkg/cachefile.cc
@@ -129,7 +129,8 @@ bool pkgCacheFile::BuildPolicy(OpProgress * /*Progress*/)
    if (_error->PendingError() == true)
       return false;
 
-   if (ReadPinFile(*Policy, Progress) == false || ReadPinDir(*Policy, Progress) == false)
+   if (ReadPinFile(*Policy) == false || ReadPinDir(*Policy) == false)
+           // if (ReadPinFile(*Policy, Progress) == false || ReadPinDir(*Policy, Progress) == false)
       return false;
 
    return true;
