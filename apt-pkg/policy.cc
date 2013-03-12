05608a28fb894797b9cc58c3dd0c07c3f243f171
Michal Maruska <mmaruska@gmail.com>
2013-03-12 22:22:13 +0100

    Try hard to Trace when the pinning info is read-in.
    Needs passing down opProgress

--- apt-pkg/policy.cc
+++ apt-pkg/policy.cc
@@ -316,7 +316,7 @@ APT_PURE signed short pkgPolicy::GetPriority(pkgCache::PkgFileIterator const &Fi
    given dir is empty the dir set in Dir::Etc::PreferencesParts is used.
    Note also that this method will issue a warning if the dir does not
    exists but it will return true in this case! */
-bool ReadPinDir(pkgPolicy &Plcy,string Dir)
+bool ReadPinDir(pkgPolicy &Plcy,string Dir, OpProgress *Progress)
 {
    if (Dir.empty() == true)
       Dir = _config->FindDir("Dir::Etc::PreferencesParts", "/dev/null");
@@ -338,7 +338,7 @@ bool ReadPinDir(pkgPolicy &Plcy,string Dir)
    // Read the files
    bool good = true;
    for (vector<string>::const_iterator I = List.begin(); I != List.end(); ++I)
-      good = ReadPinFile(Plcy, *I) && good;
+       good = ReadPinFile(Plcy, *I, Progress) && good;
    return good;
 }
 									/*}}}*/
@@ -348,7 +348,7 @@ bool ReadPinDir(pkgPolicy &Plcy,string Dir)
    but right now that is the only stuff I have to store. Later there will
    have to be some kind of combined super parser to get the data into all
    the right classes.. */
-bool ReadPinFile(pkgPolicy &Plcy,string File)
+bool ReadPinFile(pkgPolicy &Plcy,string File, OpProgress *Progress)
 {
    if (File.empty() == true)
       File = _config->FindFile("Dir::Etc::Preferences");
@@ -423,6 +423,12 @@ bool ReadPinFile(pkgPolicy &Plcy,string File)
       if (priority == 0)
       {
          return _error->Error(_("No priority (or zero) specified for pin"));
+      } else {
+#if 0
+         Progress->OverallProgress(0, 10, 1 "maruska: pin value ");,
+         std::clog << "maruska: pin value " << priority << std::endl;
+#endif
+         // _error->Warning(_("maruska: pin value %s"), priority);
       }
 
       istringstream s(Name);
