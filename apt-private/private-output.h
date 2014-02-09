8121d0fb21934b419d6e35fdc04d4d2e8386f8c9
Michal Maruska <mmaruska@gmail.com>
2014-02-09 23:24:50 +0100

    default value for exported function

--- apt-private/private-output.h
+++ apt-private/private-output.h
@@ -32,7 +32,7 @@ APT_PUBLIC void ShowBroken(std::ostream &out, CacheFile &Cache, bool const Now);
 APT_PUBLIC void ShowBroken(std::ostream &out, pkgCacheFile &Cache, bool const Now);
 
 APT_PUBLIC bool ShowList(std::ostream &out, std::string Title, std::string List,
-              std::string VersionsList);
+              std::string VersionsList, const char* pkgname_color = warn_color);
 void ShowNew(std::ostream &out,CacheFile &Cache);
 void ShowDel(std::ostream &out,CacheFile &Cache);
 void ShowKept(std::ostream &out,CacheFile &Cache);
