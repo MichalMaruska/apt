8034fc8ba8d7614604fe8343c9263acf731e999f
Michal Maruska <mmaruska@gmail.com>
2014-03-05 02:46:00 +0100

    Colorize the package names -- intro

---
+++ apt-private/colors.h
@@ -0,0 +1,19 @@
+
+#ifndef __COLORS_H__
+#define __COLORS_H__
+
+#define escape_sequence "\x1b"
+
+#define info_color  "\x1b[47;30m"
+#define warn_color "\x1b[01;32;41m"
+
+#define install_color "\x1b[33;01m"
+#define remove_color "\x1b[38;5;160m"
+#define blocked_color "\x1b[47;31m"
+
+#define version_color "\x1b[38;5;226m"
+
+#define color_reset "\x1b[0m"
+
+
+#endif /* __COLORS_H__ */
