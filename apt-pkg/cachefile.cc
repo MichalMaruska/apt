dde7de539fe33b8a16d2efaae35556e6516fd1bf
Michal Maruska <mmaruska@gmail.com>
2017-08-18 12:29:21 +0200

    fix

--- apt-pkg/cachefile.cc
+++ apt-pkg/cachefile.cc
@@ -161,8 +161,8 @@ bool pkgCacheFile::BuildPolicy(OpProgress * /*Progress*/)
    if (_error->PendingError() == true)
       return false;
 
-   ReadPinFile(*Policy, Progress);
-   ReadPinDir(*Policy, Progress);
+   ReadPinFile(*Policy);
+   ReadPinDir(*Policy);
    // if (ReadPinFile(*Policy, Progress) == false || ReadPinDir(*Policy, Progress) == false)
 
    this->Policy = Policy.release();
