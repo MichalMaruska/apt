9d92b1169a5ecc8267badce833c9f401e9bd1fe6
Michal Maruska <mmaruska@gmail.com>
2021-09-13 18:55:34 +0200

    Use color when showing upgrade away from "maruska" version
    "current => candidate" string is colorized in case current is *maruska & candiate is not

--- apt-private/private-output.cc
+++ apt-private/private-output.cc
@@ -27,6 +27,8 @@
 #include <sstream>
 
 #include <apti18n.h>
+
+#include "colors.h"
 									/*}}}*/
 
 using namespace std;
@@ -768,8 +770,16 @@ std::string CurrentToCandidateVersion(pkgCacheFile * const Cache, pkgCache::PkgI
       if (not CandVerIter.end())
 	 CandVer = CandVerIter.VerStr();
    }
-   return  CurVer + " => " + CandVer;
+
+   auto line = CurVer + " => " + CandVer;
+
+   if ((std::strstr((*Cache)[Pkg].CurVersion, "maruska") != NULL)
+       && (std::strstr((*Cache)[Pkg].CandVersion, "maruska") == NULL))
+     return std::string(blocked_color) + line + color_reset;
+   else
+     return line;
 }
+
 std::function<std::string(pkgCache::PkgIterator const &)> CurrentToCandidateVersion(pkgCacheFile * const Cache)
 {
    return std::bind(static_cast<std::string(*)(pkgCacheFile * const, pkgCache::PkgIterator const&)>(&CurrentToCandidateVersion), Cache, std::placeholders::_1);
